ba1a4fc1d765ac5df54b70470b3c95437fa919eb
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-08-01 14:50:39 -0300

    Add callbacks to fs functions (fix #5)
    - Fix test script

--- src/files/ng-add-pug-loader.js
+++ src/files/ng-add-pug-loader.js
@@ -7,16 +7,21 @@ const commonCliConfig = '__COMMON_CLI_CONFIG_PATH__';
 const pugRule = '__PUG_RULE__';
 
 fs.readFile(commonCliConfig, (err, data) => {
-  if (err) { throw err; }
+  if (err) throw err;
 
   const configText = data.toString();
   // make sure we don't add the rule if it already exists
-  if (configText.indexOf(pugRule) > -1) { return; }
+  if (configText.indexOf(pugRule) > -1) return;
 
   // Insert the pug webpack rule
   const position = configText.indexOf('rules: [') + 8;
   const output = [configText.slice(0, position), pugRule, configText.slice(position)].join('');
+  
   const file = fs.openSync(commonCliConfig, 'r+');
-  fs.writeFile(file, output);
-  fs.close(file);
+  fs.writeFile(file, output, error => {
+    if (error)
+      console.error("An error occurred while overwriting Angular CLI's Webpack config");
+
+    fs.close(file, () => {});
+  });
 });
