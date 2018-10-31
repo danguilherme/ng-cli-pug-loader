5053114f6d44cabddffdc5b2b80a24d04d3fff15
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-10-31 10:14:32 +0100

    Fix postinstall execution on Node 10 (#7)
    * Add callbacks to fs functions (fix #5)
    
    - Fix test script
    
    * Add Node 10 on CI tests

--- src/files/ng-add-pug-loader.js
+++ src/files/ng-add-pug-loader.js
@@ -7,7 +7,7 @@ const commonCliConfig = '__COMMON_CLI_CONFIG_PATH__';
 const pugRules = '__PUG_RULES__';
 
 fs.readFile(commonCliConfig, (err, data) => {
-  if (err) { throw err; }
+  if (err) throw err;
 
   const configText = data.toString();
   // make sure we don't add the rule if it already exists
@@ -17,6 +17,10 @@ fs.readFile(commonCliConfig, (err, data) => {
   const position = configText.indexOf('rules: [') + 8;
   const output = [configText.slice(0, position), pugRules, configText.slice(position)].join('');
   const file = fs.openSync(commonCliConfig, 'r+');
-  fs.writeFile(file, output);
-  fs.close(file);
+  fs.writeFile(file, output, error => {
+    if (error)
+      console.error("An error occurred while overwriting Angular CLI's Webpack config");
+
+    fs.close(file, () => {});
+  });
 });
