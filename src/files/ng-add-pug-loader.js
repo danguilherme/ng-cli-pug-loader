aaae26d3f10706d38d877213c5a83843577f6775
Joshua Nitschke <33307206+JoshuaNitschke@users.noreply.github.com>
2019-06-11 15:12:23 -0700

    Set directTemplateLoading to False for Angular 8

--- src/files/ng-add-pug-loader.js
+++ src/files/ng-add-pug-loader.js
@@ -24,3 +24,28 @@ fs.readFile(commonCliConfig, (err, data) => {
     fs.close(file, () => {});
   });
 });
+
+/**
+ * Set's directTemplateLoading: false to allow custom pug template loader to work
+ * @see https://github.com/angular/angular-cli/issues/14534
+ */
+const typescriptCliConfig = '__TYPESCRIPT_CLI_CONFIG_PATH__';
+
+fs.readFile(typescriptCliConfig, (err, data) => {
+  if (err) { throw err; }
+
+  const typescriptText = data.toString();
+  
+  // update the setting
+  const output = typescriptText.replace('directTemplateLoading: true,', 'directTemplateLoading: false,');
+
+  // rewrite the file
+  const file2 = fs.openSync(typescriptCliConfig, 'r+');  
+  fs.writeFile(file2, output, error => {
+    if (error)
+      console.error("An error occurred while overwriting Angular CLI's Webpack config");
+
+    fs.close(file2, () => {});
+  });
+});
+
