624fd435a41e34610819f325366052436fdf6fb3
Joshua Nitschke <33307206+JoshuaNitschke@users.noreply.github.com>
2019-06-11 15:27:58 -0700

    Update direct template loading to false for Angular 8
    I don't have time to figure out how to test this with the ng add *** utility, so hopefully it works. 
    I was able to test my version of ng-add-pug-loader.js (after filling in the templated parts)

--- src/schematics/ng-add/index.ts
+++ src/schematics/ng-add/index.ts
@@ -11,7 +11,8 @@ import { throwError } from 'rxjs';
 import { addDependencyToPackageJson, addScriptToPackageJson } from '../../utils/package';
 import { devDependencies } from './dependencies';
 
-const TARGET_CONFIG_PATH = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js';
+const TARGET_COMMON_CONFIG_PATH = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js';
+const TARGET_TYPESCRIPT_CONFIG_PATH = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/typescript.js';
 const NG_ADD_PUG_LOADER_SCRIPT_NAME = 'ng-add-pug-loader.js';
 
 /**
@@ -60,21 +61,29 @@ function addLoadersToPackageJson() {
  */
 function addPugRules() {
   return (tree: Tree) => {
-    const configFile = tree.read(TARGET_CONFIG_PATH);
-    if (!configFile)
+    const commonConfigFile = tree.read(TARGET_COMMON_CONFIG_PATH);
+    const typescriptConfigFile = tree.read(TARGET_TYPESCRIPT_CONFIG_PATH);
+      
+    if (!commonConfigFile || !typescriptConfigFile)
       return throwError(new Error(`CLI's Webpack config was not found. Try running \`npm install\` before running this tool.`));
-
-    const configText = configFile.toString('utf-8');
+    
+    const commonConfigText = commonConfigFile.toString('utf-8');
     const strPugRules = getPugLoaderRules();
 
     // make sure we don't add the rule if it already exists
-    if (configText.indexOf(strPugRules) > -1) { return; }
+    if (commonConfigText.indexOf(strPugRules) > -1) { return; }
 
     // We made it this far, let's insert that pug webpack rule
-    const position = configText.indexOf('rules: [') + 8;
-    const output = [configText.slice(0, position), `\n${strPugRules}`, configText.slice(position)].join('');
+    const position = commonConfigText.indexOf('rules: [') + 8;
+    const commonOutput = [commonConfigText.slice(0, position), `\n${strPugRules}`, commonConfigText.slice(position)].join('');
 
-    tree.overwrite(TARGET_CONFIG_PATH, output);
+    tree.overwrite(TARGET_COMMON_CONFIG_PATH, commonOutput);
+
+    // now we update the typescript config file to turn off direct template loading
+    const typescriptConfigText = typescriptConfigFile.toString('utf-8');
+    if (typescriptConfigText.indexOf('directTemplateLoading: false,') > -1) { return; }
+    const typescriptOutput = typescriptConfigText.replace('directTemplateLoading: true,', 'directTemplateLoading: false,');
+    tree.overwrite(TARGET_COMMON_CONFIG_PATH, typescriptOutput)
   }
 }
 
@@ -98,7 +107,8 @@ function addScriptToProject() {
     const scriptContent = files.read(NG_ADD_PUG_LOADER_SCRIPT_NAME)!.toString('utf-8');
 
     const modifiedContent = replaceVars(scriptContent, {
-      COMMON_CLI_CONFIG_PATH: TARGET_CONFIG_PATH,
+      COMMON_CLI_CONFIG_PATH: TARGET_COMMON_CONFIG_PATH,
+      TYPESCRIPT_CLI_CONFIG_PATH: TARGET_TYPESCRIPT_CONFIG_PATH,
       PUG_RULES: getPugLoaderRules()
     });
 
