9dc676763345c6405f7a016dd46919c382c72488
Joshua Nitschke <33307206+JoshuaNitschke@users.noreply.github.com>
2019-06-13 10:13:46 -0700

    Update for Angular 8 (#22)
    * Set directTemplateLoading to False for Angular 8
    
    * Update direct template loading to false for Angular 8
    
    * Fix the existing broken unit tests
    Add a unit test for Angular 8
    Add a check to maintain Angular 6/7 compatability per PR feedback
    
    * add "update needed to typescript.js check" to ng-add-pug-loader.js script
    make changes requested in PR:
    * fix typo on unit test description
    * refactor variable names relating to version of Angular
    * alter mocked unit test file utilize subset of typescript.js

--- src/files/ng-add-pug-loader.js
+++ src/files/ng-add-pug-loader.js
@@ -24,3 +24,30 @@ fs.readFile(commonCliConfig, (err, data) => {
     fs.close(file, () => {});
   });
 });
+
+/**
+ * Set's directTemplateLoading: false to allow custom pug template loader to work
+ * @see https://github.com/angular/angular-cli/issues/14534
+ */
+const typescriptCliConfig = '__TYPESCRIPT_CLI_CONFIG_PATH__';
+
+fs.readFile(typescriptCliConfig, (err, data) => {
+  if (err) { throw err; }
+
+  const typescriptText = data.toString();
+
+  // check if needed to be set or already set
+  if (typescriptText.indexOf('directTemplateLoading') === -1 || typescriptText.indexOf('directTemplateLoading: false,') > -1) { return; }
+  
+  // update the setting
+  const output = typescriptText.replace('directTemplateLoading: true,', 'directTemplateLoading: false,');
+
+  // rewrite the file
+  const file2 = fs.openSync(typescriptCliConfig, 'r+');  
+  fs.writeFile(file2, output, error => {
+    if (error)
+      console.error("An error occurred while overwriting Angular CLI's Webpack config");
+
+    fs.close(file2, () => {});
+  });
+});
--- src/utils/testing.ts
+++ src/utils/testing.ts
@@ -3,6 +3,48 @@ import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/te
 import { Tree } from '@angular-devkit/schematics';
 
 const collectionPath = join('./node_modules/@schematics/angular/collection.json');
+const commonJsFile = 
+`({
+    rules: [
+        { test: /.anything$/, loader: 'any-loader' }
+    ]
+})`
+
+const angular6TypescriptJsFile =
+`const pluginOptions: AngularCompilerPluginOptions = {
+    mainPath: useMain ? path.join(root, buildOptions.main) : undefined,
+    ...i18nFileAndFormat,
+    locale: buildOptions.i18nLocale,
+    platform: buildOptions.platform === 'server' ? PLATFORM.Server : PLATFORM.Browser,
+    missingTranslation: buildOptions.i18nMissingTranslation,
+    sourceMap: buildOptions.sourceMap.scripts,
+    additionalLazyModules,
+    hostReplacementPaths,
+    nameLazyFiles: buildOptions.namedChunks,
+    forkTypeChecker: buildOptions.forkTypeChecker,
+    contextElementDependencyConstructor: require('webpack/lib/dependencies/ContextElementDependency'),
+    logger: wco.logger,
+    ...options,
+  };`
+
+const angular7orGreaterTypescriptJsFile =
+`const pluginOptions: AngularCompilerPluginOptions = {
+    mainPath: useMain ? path.join(root, buildOptions.main) : undefined,
+    ...i18nFileAndFormat,
+    locale: buildOptions.i18nLocale,
+    platform: buildOptions.platform === 'server' ? PLATFORM.Server : PLATFORM.Browser,
+    missingTranslation: buildOptions.i18nMissingTranslation,
+    sourceMap: buildOptions.sourceMap.scripts,
+    additionalLazyModules,
+    hostReplacementPaths,
+    nameLazyFiles: buildOptions.namedChunks,
+    forkTypeChecker: buildOptions.forkTypeChecker,
+    contextElementDependencyConstructor: require('webpack/lib/dependencies/ContextElementDependency'),
+    logger: wco.logger,
+    directTemplateLoading: true,
+    ...options,
+  };`
+
 
 /**
  * Create a base app used for testing.
@@ -30,13 +72,12 @@ export function createTestApp(): UnitTestTree {
   }, tree);
 }
 
-export function createCommonWebpackConfig(host: Tree) {
-  host.create(
-    'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js',
-    `
-({
-  rules: [
-    { test: /.anything$/, loader: 'any-loader' }
-  ]
-})`);
+export function createCommonWebpackConfigForAngular6(host: Tree) {
+    host.create("node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js", commonJsFile);
+    host.create("node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/typescript.js", angular6TypescriptJsFile);
+}
+
+export function createCommonWebpackConfigForAngular7orGreater(host: Tree) {
+    host.create("node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js", commonJsFile);
+    host.create("node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/typescript.js", angular7orGreaterTypescriptJsFile);
 }
